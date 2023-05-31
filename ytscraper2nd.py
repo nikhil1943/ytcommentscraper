b742e33be4b292ef9eb43cf6daac90b0f365d9dc
nikhil1943 <98119451+nikhil1943@users.noreply.github.com>
2023-05-31 21:28:21 +0530

    main executable file

---
+++ ytscraper2nd.py
@@ -0,0 +1,45 @@
+import os
+from googleapiclient.discovery import build
+from dotenv import load_dotenv
+from utils.comments import process_comments, make_csv
+
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
+
+youtube = build("youtube","v3",developerKey=API_KEY)
+
+def comment_threads(vidID, to_csv):
+    
+    comments_list = []
+    
+    request = youtube.commentThreads().list(
+    part='id,replies,snippet',
+    videoId = vidID
+    )
+    response = request.execute()
+    comments_list.extend(process_comments(response['items']))
+    
+    while response.get('nextPageToken', None):
+        request = youtube.commentThreads().list(
+            part='id,replies,snippet',
+            videoId=vidID,
+            pageToken=response['nextPageToken']
+        )
+        response = request.execute()
+        comments_list.extend(process_comments(response['items']))
+        
+    print(f"Finished fetching comments for {vidID}. {len(comments_list)} comments found.")
+    
+    if to_csv:
+        make_csv(comments_list, vidID)
+    
+    return comments_list
+
+
+def main():
+    comment_threads('KBZfnt80s54',to_csv = True)
+
+
+if __name__ == "__main__":
+    main()
