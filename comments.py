069d54afb739029114515665df7072a42e4ce1c8
nikhil1943 <98119451+nikhil1943@users.noreply.github.com>
2023-05-31 21:30:33 +0530

    Add files via upload

---
+++ comments.py
@@ -0,0 +1,42 @@
+import csv
+
+comments = []
+
+
+def process_comments(response_items,csv_output=False):
+    for res in response_items:
+        #replies
+        if 'replies' in res.keys():
+            for reply in res['replies']['comments']:
+                comment = reply['snippet']
+                comment['commentId'] = reply['id']
+                comments.append(comment)
+        
+        #non replies
+
+        else:
+            comment = {}
+            comment['snippet'] = res['snippet']['topLevelComment']['snippet']
+            comment['snippet']['parentId'] = None
+            comment['snippet']['commentId'] = res['snippet']['topLevelComment']['id']
+            
+            comments.append(comment['snippet'])
+        
+    if csv_output:
+        make_csv(comments)
+    
+    return comments        
+
+def make_csv(comments, channelID=None):
+    header = comments[0].keys()
+    
+    if channelID:
+        filename = f'comments_{channelID}.csv'
+    else:
+        filename = f'comments.csv'
+        
+    with open(filename,'w',encoding='utf8',newline='') as f:
+        writer = csv.DictWriter(f, fieldnames=header, extrasaction='ignore')
+        writer.writeheader()
+        writer.writerows(comments)
+        
